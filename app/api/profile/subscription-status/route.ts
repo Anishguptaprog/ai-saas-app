fa5c6b23c25930d0eb8af6e1557b6872eeee1792
Anishguptaprog <anishajaygupta21@gmail.com>
2025-03-06 08:12:06 +0530

    final commit

---
+++ app/api/profile/subscription-status/route.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { prisma } from "@/app/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { stat } from "fs";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    try{
+        const clerkUser = await currentUser()
+        if(!clerkUser?.id){
+            return NextResponse.json({error:"Unauthorized"});
+        }
+        const profile = await prisma.profile.findUnique({
+            where:{userId:clerkUser.id},
+            select:{subscriptionTier:true}
+        });
+        if(!profile){
+            return NextResponse.json({error:"Profile not found"});
+        }
+        return NextResponse.json({subscription:profile});
+    } catch (error:unknown){
+        return NextResponse.json({error:"Internal Error"},{status:500});
+    }
+}
